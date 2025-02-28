901b756f13459637c0774dfdedb642c73886e59c
ComPleHN <ghn108@163.com>
2025-02-28 18:32:59 +0800

    fix： Compatible modules and export
    兼容

--- src/sessionGuard.js
+++ src/sessionGuard.js
@@ -2,46 +2,51 @@
 const SESSION_KEY = '__session_guard__';
 
 function defineSessionGuard(key, fetcher) {
-  const storageKey = `__${key}_session__`;
-  
-  // 单例处理
-  if (window[SESSION_KEY]) {
-    window[SESSION_KEY] = {};
-  }
-  
-  const instance = {
-    async get() {
-      try {
-        const raw = sessionStorage.getItem(storageKey);
-        if (!raw) return this.fetchFresh();
-        
-        sessionStorage.removeItem(storageKey); // 无论成功与否都删除
-        const decoded = atob(raw);
-        return JSON.parse(decoded);
-      } catch {
-        return this.fetchFresh();
-      }
-    },
+    const storageKey = `__${key}_session__`;
 
-    async fetchFresh() {
-      const data = await fetcher();
-      this.write(data);
-      return data;
-    },
-
-    write(data) {
-        console.log('写入数据到sessionStorage',data);
-        
-      const encoded = btoa(JSON.stringify(data));
-      sessionStorage.setItem(storageKey, encoded);
+    // 单例处理
+    if (window[SESSION_KEY]) {
+        window[SESSION_KEY] = {};
     }
-  };
 
-  window[SESSION_KEY] = { [key]: instance };
-  return instance;
+    const instance = {
+        async get() {
+            try {
+                const raw = sessionStorage.getItem(storageKey);
+                if (!raw) return this.fetchFresh();
+
+                sessionStorage.removeItem(storageKey); // 无论成功与否都删除
+                const decoded = atob(raw);
+                return JSON.parse(decoded);
+            } catch {
+                return this.fetchFresh();
+            }
+        },
+
+        async fetchFresh() {
+            const data = await fetcher();
+            this.write(data);
+            return data;
+        },
+
+        write(data) {
+            console.log('写入数据到sessionStorage', data);
+
+            const encoded = btoa(JSON.stringify(data));
+            sessionStorage.setItem(storageKey, encoded);
+        }
+    };
+
+    window[SESSION_KEY] = { [key]: instance };
+    return instance;
 }
 
+// 浏览器环境支持
 if (typeof window !== 'undefined') {
-  window.defineSessionGuard = defineSessionGuard; 
+    window.defineSessionGuard = defineSessionGuard;
 }
-// export { defineSessionGuard };
+
+// 模块化环境支持
+if (typeof exports !== 'undefined') {
+    module.exports = { defineSessionGuard };
+}
